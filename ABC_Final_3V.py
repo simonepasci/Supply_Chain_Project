f66505579900eb747186c22432386e386a21a1bf
simone.pasci <simone.pasci@gmail.com>
2021-06-08 17:57:18 +0200

    changed ABC Method to running total vs quartile

--- ABC_Final_3V.py
+++ ABC_Final_3V.py
@@ -10,6 +10,12 @@ data = pd.read_csv('ABC-XYZ-data_3.csv',sep =';')
 
 def ABC_analysis(df):
     
+   
+    # remove Quantity and Timeperiod columns
+
+    df =df.drop(columns = ['Timeperiod', 'Quantity'])
+
+
     # groupby PT, Plant and Materials
   
     grouped_df = df.groupby(by=['PT', 'Plant', 'Material']).sum()
@@ -19,27 +25,34 @@ def ABC_analysis(df):
     
     grouped_df = grouped_df.sort_values(by=['PT','Plant','Revenue'], ascending=[True, True, False])
     
-    #applied running totals at 3 previous dimentions
+    #applied running totals at 3 dimentions (level = when runningtotal restarts)
+    
+    
+    grouped_df['Running'] = grouped_df["Revenue"].groupby(level=['PT',"Plant"]).cumsum()
 
-
-    grouped_df['Running'] = grouped_df["Revenue"].groupby(level=[0]).cumsum()
+    #grouped_df['Running'] = grouped_df["Revenue"].groupby(level=[0]).cumsum()
     #new column subtotals
    
     
-    
 
-    grouped_df["Subtotals"] = ' '
+    
+    
+    #Calculating Subtotals of PT and Plants combined
+    grouped_df["subt2"]=""
+    grouped_df['subt2'] = grouped_df.groupby(['PT','Plant'])['Revenue'].sum()
 
     #Calculating percentiles on subtotals
 
 
-    grouped_df["Subtotals"] = grouped_df.groupby(['PT','Plant'])['Running'].rank(pct=True).mul(100)
+    grouped_df["Subtotals"] = ' '
+    grouped_df['Subtotals'] = grouped_df['Running']/grouped_df['subt2']
+    # grouped_df["Subtotals"] = grouped_df.groupby(by=['PT','Plant'])['Running'].rank(pct=True).mul(100)
    
     # Checking the ABC ranking of the PT and Plant and Categorising into class A,B,C and splitting based on 80%-15%-5%
     
 
     grouped_df["ABC"] = ' '
-    grouped_df["ABC"] = grouped_df["Subtotals"].apply(lambda x: "A" if x <80 else ('B' if x < 95 else 'C'))
+    grouped_df["ABC"] = grouped_df["Subtotals"].apply(lambda x: "A" if x <0.801 else ('B' if x < 0.9501 else 'C'))
 
     #changing ABC type column from object into categoty to plot it
     grouped_df['ABC'] = grouped_df['ABC'].astype('category')
@@ -48,8 +61,19 @@ def ABC_analysis(df):
     return grouped_df
 
 ABC = ABC_analysis (data)
-print(ABC)  
+print(ABC)
 print(ABC.dtypes)
 
+# plot
 
 
+
+
+
+plt.bar(ABC["ABC"], ABC["Revenue"])
+
+plt.legend('Revenue')
+plt.xlabel("ABC")
+plt.ylabel("Revenue in USD")
+plt.title("ABC Analysis")
+plt.show()
